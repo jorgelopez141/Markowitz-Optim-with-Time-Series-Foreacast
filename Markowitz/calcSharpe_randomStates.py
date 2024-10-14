72dddde338da371c0d908aa99abab1101f97b37d
jorgelopez141 <jdlopez13@gmail.com>
2024-10-13 22:16:03 -0500

    created best and worst portfolios and plotted in calcSharpe_randomStates

---
+++ Markowitz/calcSharpe_randomStates.py
@@ -0,0 +1,146 @@
+#%%
+import os
+# Change to the directory of the current file
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import numpy as np 
+import pandas as pd 
+from numbersAddTo1 import random_gen_weights
+from unitCircle import unitCircle as unitCircleDf
+import matplotlib.pyplot as plt
+from function_filePrep import tickerList, download_data,missing_days_andIndexTimeZone, to_month_and_add_monthYear_columns
+np.random.seed(1986)
+
+n_rows = 10000
+n_cols = 30
+data = []
+data_uniform= []
+
+for _ in range(n_rows):
+    row = random_gen_weights(n_cols)       
+    data.append(row)
+
+#downloading only last year as I believe is more representative of the picture going forward
+df_weights = pd.DataFrame(data)
+df_ticker_price = download_data(list_stocks=tickerList,start_date = '2023-08-01', end_date = '2024-09-30')
+df_ticker_price1=missing_days_andIndexTimeZone(df_ticker_price)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+#%%
+## 41 rows of daily pct change in aug and sept 2024
+training_1y_endJul2024=df_ticker_price1[:"2024-07-31"].copy()
+
+training_pct_change=training_1y_endJul2024.pct_change().dropna()
+
+#%%
+port_means= []
+port_sd = []
+for row in df_weights.values: 
+    iter=training_pct_change.dot(row)
+    i_mean=iter.mean()
+    i_sd = iter.std()
+    port_means.append(i_mean*41) # times 41 to get expected return in 41 days (2 months)
+    port_sd.append(i_sd * np.sqrt(41))
+    
+#%%
+#renaming columns and adding sharpe ratio
+df_weights.columns = training_pct_change.columns
+df_weights['port_return'] = port_means
+df_weights['port_risk'] = port_sd
+df_weights['sharpe'] = df_weights['port_return']/df_weights['port_risk']
+#%%
+best_port=df_weights.sort_values('sharpe',ascending=False).reset_index(drop=True).iloc[0,:-3]
+#%%
+best_port.sort_values().plot(kind='barh')
+
+#%%
+# GRAPH of SECURITIES 
+df_weights.\
+    sort_values('sharpe',ascending=False).reset_index(drop=True).\
+    iloc[:1000,:].\
+        loc[:,"AAPL":"WMT"].\
+            rank(axis=1, method='max',ascending=False).\
+            apply(lambda x: np.count_nonzero(x<=5),axis=0).sort_values().plot(kind='barh')
+plt.suptitle('Securities with Largest Weight (Rank 1-5) in')
+plt.title('1,000 Top Performing Portfolios')
+
+
+#%%
+
+df_weights.\
+    sort_values('sharpe',ascending=True).reset_index(drop=True).\
+    iloc[:1000,:].\
+        loc[:,"AAPL":"WMT"].\
+            rank(axis=1, method='max',ascending=False).\
+            apply(lambda x: np.count_nonzero(x<=5),axis=0).sort_values().plot(kind='barh')
+plt.suptitle('Securities with Largest Weight (Rank 1-5) in')
+plt.title('1,000 Bottom Performing Portfolio')
+
+#%%
+
+# PLOTTING UNICIRCLE
+
+unitCircleDf['securities'] =\
+df_weights.\
+    sort_values('sharpe',ascending=False).reset_index(drop=True).\
+    iloc[:1000,:].\
+        loc[:,"AAPL":"WMT"].\
+            rank(axis=1, method='max',ascending=False).\
+            apply(lambda x: np.count_nonzero(x<=5),axis=0).sort_values().index
+
+
+#%%
+nbr_portfolios = 100
+
+best1000portfolios = df_weights.\
+    sort_values('sharpe',ascending=False).reset_index(drop=True).\
+    iloc[:nbr_portfolios,:]
+
+worst1000portfolios = df_weights.\
+    sort_values('sharpe',ascending=True).reset_index(drop=True).\
+    iloc[:nbr_portfolios,:]
+
+bestPort_coord = pd.DataFrame(
+    {
+        'best_length': [np.array(x).dot(unitCircleDf.lengths) for x in best1000portfolios.loc[:,"AAPL":"WMT"].values],
+        'best_height': [np.array(x).dot(unitCircleDf.heights) for x in best1000portfolios.loc[:,"AAPL":"WMT"].values]
+    }
+)
+
+worstPort_coord = pd.DataFrame(
+    {
+        'worst_length': [np.array(x).dot(unitCircleDf.lengths) for x in worst1000portfolios.loc[:,"AAPL":"WMT"].values],
+        'worst_height': [np.array(x).dot(unitCircleDf.heights) for x in worst1000portfolios.loc[:,"AAPL":"WMT"].values]
+    }
+)
+
+# plotting top n portfolios (blue), and bottom (1000) portfolios
+
+
+plt.scatter(x=unitCircleDf.lengths, y=unitCircleDf.heights, s=5, color='b')
+# Add labels
+for i, txt in enumerate(unitCircleDf['securities']):
+    plt.annotate(txt, (unitCircleDf['lengths'][i], unitCircleDf['heights'][i]))
+
+plt.scatter(x=bestPort_coord.best_length, y=bestPort_coord.best_height, s=1, color='blue', label = 'Best')
+plt.scatter(x=worstPort_coord.worst_length, y=worstPort_coord.worst_height, s=1, color='red', label = 'Worst')
+plt.annotate('DJI', (0, 0))
+# Adding legend with bigger dots
+plt.legend(markerscale=5)    
+
+
+
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5,1.5)
+plt.title('Unit Circle')
+plt.show()
+
+
+#%%
+
+plt.scatter(x=best1000portfolios.port_return, y=best1000portfolios.port_risk, color='blue', label='best')
+plt.scatter(x=worst1000portfolios.port_return, y=worst1000portfolios.port_risk, color='red', label='worst')
+plt.title('Portfolio Return vs Portfolio Risk')
+plt.xlabel('Portfolio Return')
+plt.ylabel('Portfolio Risk')
+plt.show()
